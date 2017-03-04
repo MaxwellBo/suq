166da5528dad278e5b2f2d686e02f8fb5f51ce3d
Max Bo <max@maxbo.me>
2017-03-04 14:14:35 +1000

    Rename method

--- suq/__init__.py
+++ suq/__init__.py
@@ -11,7 +11,7 @@ def hello():
 
 # v http://flask.pocoo.org/docs/0.12/patterns/apierrors/
 @app.errorhandler(APIException)
-def handle_invalid_usage(error):
+def handle_thrown_api_exceptions(error):
     response = jsonify(error.to_dict())
     # ^ http://flask.pocoo.org/docs/0.12/api/#flask.json.jsonify
     response.status_code = error.status_code
