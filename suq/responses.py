ea76da58265bac0b1afb133b4112116fe2a28cf9
Max Bo <max@maxbo.me>
2017-03-04 13:07:15 +1000

    Documentation

--- suq/responses.py
+++ suq/responses.py
@@ -1,3 +1,6 @@
+"""
+http://www.ietf.org/rfc/rfc2616.txt
+"""
 class APIException(Exception):
     def __init__(self, status_code, message, payload=None):
         self.status_code = status_code
@@ -10,6 +13,10 @@ class APIException(Exception):
             rv["payload"] = self.payload
         return rv
 
+"""
+The server encountered an unexpected condition which prevented it
+   from fulfilling the request.
+"""
 class InternalServerError(APIException):
     def __init__(self, message="Internal Server Error", payload=None):
         super().__init__(status_code=500, message=message, payload=payload)
