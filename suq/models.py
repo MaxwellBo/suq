042442577e0e316fd00b6fad88b7437145d846d2
Max Bo <max@maxbo.me>
2017-03-06 14:59:10 +1000

    Define types

--- suq/models.py
+++ suq/models.py
@@ -1,4 +1,6 @@
 from icalendar import Calendar, Event
+from typing import List, Tuple
+from datetime import datetime
 
 # http://icalendar.readthedocs.io/en/latest/usage.html#file-structure
 def load_calendar(filename: str) -> Calendar:
@@ -8,9 +10,8 @@ def load_calendar(filename: str) -> Calendar:
         return cal
 
 def print_calendar(cal: Calendar) -> None:
-    for component in cal.walk():
-        if component.name == "VEVENT":
-            print_event(component)
+    for i in events(cal):
+        print_event(i)
 
 def print_event(event: Event) -> None:
     dtstart, dtend = event.get('dtstart'), event.get('dtend')
@@ -21,8 +22,14 @@ def print_event(event: Event) -> None:
     # print("dtstamp: ", componentevent.get('dtstamp').dt)
     # Don't know what this last one is or whether it's useful
 
+def events(cal: Calendar):
+    return [ i for i in cal.walk() if i.name == "VEVENT" ]
+
+Break = Tuple[datetime, datetime]
+
+def get_breaks(cal: Calendar) -> List[Break]:
+    pass
 
 if __name__ == "__main__":
     cal = load_calendar("test.ics")
     print_calendar(cal)
-
