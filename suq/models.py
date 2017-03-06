29d380683bf354d6849655e9978390551c6b144d
Max Bo <max@maxbo.me>
2017-03-06 12:46:10 +1000

    Seperate event printing into seperate file

--- suq/models.py
+++ suq/models.py
@@ -10,14 +10,19 @@ def load_calendar(filename: str) -> Calendar:
 def print_calendar(cal: Calendar) -> None:
     for component in cal.walk():
         if component.name == "VEVENT":
-            dtstart, dtend = component.get('dtstart'), component.get('dtend')
-            # ^ http://icalendar.readthedocs.io/en/latest/_modules/icalendar/prop.html#vDDDTypes
-            print("summary: ", component.get('summary'))
-            print("dtstart: ", type(dtstart.dt), dtstart.dt)
-            print("dtend:   ", type(dtend.dt), dtend.dt)
-            # print("dtstamp: ", component.get('dtstamp').dt)
-            # Don't know what this last one is or whether it's useful
+            print_event(component)
+
+def print_event(event: Event) -> None:
+    dtstart, dtend = event.get('dtstart'), event.get('dtend')
+    # ^ http://icalendar.readthedocs.io/en/latest/_modules/icalendar/prop.html#vDDDTypes
+    print("summary: ", event.get('summary'))
+    print("dtstart: ", type(dtstart.dt), dtstart.dt)
+    print("dtend:   ", type(dtend.dt), dtend.dt)
+    # print("dtstamp: ", componentevent.get('dtstamp').dt)
+    # Don't know what this last one is or whether it's useful
+
 
 if __name__ == "__main__":
     cal = load_calendar("test.ics")
     print_calendar(cal)
+
