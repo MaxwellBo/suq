bd92f9c5fd08e30a965d2b920c8f59e2ff266763
charltongroves <charltongroves@gmail.com>
2017-03-06 15:43:16 +1000

    Added a function to merge overlapping events in the users calander

--- suq/models.py
+++ suq/models.py
@@ -1,4 +1,5 @@
 from icalendar import Calendar, Event
+import datetime
 
 # http://icalendar.readthedocs.io/en/latest/usage.html#file-structure
 def load_calendar(filename: str) -> Calendar:
@@ -13,16 +14,56 @@ def print_calendar(cal: Calendar) -> None:
             print_event(component)
 
 def print_event(event: Event) -> None:
-    dtstart, dtend = event.get('dtstart'), event.get('dtend')
+    dtstart, dtend = event.get('dtstart').dt, event.get('dtend').dt
     # ^ http://icalendar.readthedocs.io/en/latest/_modules/icalendar/prop.html#vDDDTypes
-    print("summary: ", event.get('summary'))
-    print("dtstart: ", type(dtstart.dt), dtstart.dt)
-    print("dtend:   ", type(dtend.dt), dtend.dt)
+    #print("summary: ", event.get('summary'))
+    #print("dtstart: ", type(dtstart.dt), dtstart.dt)
+    #print("dtend:   ", type(dtend.dt), dtend.dt)
     # print("dtstamp: ", componentevent.get('dtstamp').dt)
     # Don't know what this last one is or whether it's useful
+
+"""
+compress all the events in the cal to a list of starttime endtime tuples
+"""
+def cal_to_tuple(cal: Calendar):
+    events = []
+    for component in cal.walk():
+        if component.name == "VEVENT":
+            dtstart, dtend = component.get('dtstart').dt, component.get('dtend').dt
+            dtstart = dtstart.replace(tzinfo=None)
+            dtend = dtend.replace(tzinfo=None)
+            dtstart = (dtstart - datetime.datetime(1970,1,1)).total_seconds()
+            dtend = (dtend - datetime.datetime(1970,1,1)).total_seconds()
+            events.append((dtstart, dtend))
+    return sorted(events)
+
+def find_freetime(cal: Calendar):
+    #combine overlapping events
+    events = cal_to_tuple(cal) #events should be sorted by start time
+    finalEvents = [events[0]]
+
+    #grab the event you want to merge to the previous event,
+    #save its start time to c and its end time to d
+    for c, d in events[1:]:
+        #grab the most recently added event, and save its
+        #start time to a, and end time to b
+        a, b = finalEvents[-1]
+        if c<=b<d:
+            # c<=b<d: a-------b          Ans: [(a,d)]
+            #               c---d
+            finalEvents[-1] = a, d
+        elif b<c<d:  
+            # b<c<d: a-------b           Ans: [(a,b),(c,d)]
+            #                  c---d
+            finalEvents.append((c,d))
+        else:
+            # c<d<b: a-------b           Ans: [(a,b)]
+            #         c---d
+            pass
+        
 
 
 if __name__ == "__main__":
     cal = load_calendar("test.ics")
-    print_calendar(cal)
+    find_freetime(cal)
 
