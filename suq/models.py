44c9f6e3712b92ef675379c69263160b8dc923a1
Max Bo <max@maxbo.me>
2017-03-07 22:16:21 +1000

    Remove old method

--- suq/models.py
+++ suq/models.py
@@ -31,48 +31,6 @@ def events(cal: Calendar) -> List[Event_]:
     return [ Event_(i.get('summary'), i.get('dtstart').dt, i.get('dtend').dt)\
     for i in cal.walk() if i.name == "VEVENT" ]
 
-"""
-compress all the events in the cal to a list of starttime endtime tuples
-"""
-def cal_to_tuple(cal: Calendar):
-    events = []
-    for component in cal.walk():
-        if component.name == "VEVENT":
-            dtstart, dtend = component.get('dtstart').dt, component.get('dtend').dt
-            dtstart = dtstart.replace(tzinfo=None)
-            dtend = dtend.replace(tzinfo=None)
-            events.append((dtstart, dtend))
-    return sorted(events)
-
-def find_freetime(cal: Calendar):
-    #combine overlapping events
-    events = cal_to_tuple(cal) #events should be sorted by start time
-    finalEvents = [events[0]]
-
-    #grab the event you want to merge to the previous event,
-    #save its start time to c and its end time to d
-    for c, d in events[1:]:
-
-        #grab the most recently added event, and save its
-        #start time to a, and end time to b
-        a, b = finalEvents[-1]
-        if c<=b<d:
-            # c<=b<d: a-------b          Ans: [(a,d)]
-            #               c---d
-            #      =  a---------d
-            finalEvents[-1] = a, d
-        elif b<c<d:
-            # b<c<d: a-------b           Ans: [(a,b),(c,d)]
-            #                  c---d
-            #      = a-------b c---d
-            finalEvents.append((c,d))
-        else:
-            # c<d<b: a-------b           Ans: [(a,b)]
-            #         c---d
-            #      = a-------b
-            pass
-
-
 def get_breaks(cal: Calendar) -> List[Break]:
     by_start = sorted(events(cal), key=lambda i: i.start)
     by_end = sorted(events(cal), key=lambda i: i.end)
@@ -104,7 +62,6 @@ def get_breaks(cal: Calendar) -> List[Break]:
 
 if __name__ == "__main__":
     cal = load_calendar("test.ics")
-    find_freetime(cal)
 
     for (start, finish) in get_breaks(cal):
         print(start, finish)
