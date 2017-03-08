d5dbb1febb9134c929d9f486de8f67c84c63aa5d
Max Bo <max@maxbo.me>
2017-03-08 12:04:39 +1000

    Add break finding

--- suq/models.py
+++ suq/models.py
@@ -1,11 +1,11 @@
 from itertools import *
 
 from typing import List, Tuple, Dict, NewType
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 
 from icalendar import Calendar, Event # type: ignore
 
-UserID = int
+UserID = str
 
 class Period(object):
     def __init__(self, start: datetime, end: datetime) -> None:
@@ -68,24 +68,40 @@ def get_breaks(cal: Calendar) -> List[Break]:
     return [ i for i in breaks if not is_short_break(i) and not is_overnight(i) ]
 
 
-def get_breaks_now(user: UserID, friends_to_breaks: Dict[UserID, List[Break]])\
-    -> Dict[UserID, Break]:
+def get_breaks_now(user: UserID, friends_to_calendar: Dict[UserID, Calendar])-> Dict[UserID, Break]:
 
-    print(friends_to_breaks)
+    collector = {}
 
+    for (friend, calendar) in friends_to_calendar.items():
+        if user == friend:
+            # Don't check our own breaks
+            continue
+
+        get_current_breaks = [ i for i in get_breaks(calendar)\
+            if datetime.now(timezone(timedelta(hours=10))) in i ]
+
+        if len(get_current_breaks) != 0:
+            assert(len(get_current_breaks) == 1)
+            collector[friend] = get_current_breaks[0]
+
+    return collector
 
 if __name__ == "__main__":
-    maxID, max = 0, load_calendar("../calendars/max.ics")
-    charlieID, charlie = 1, load_calendar("../calendars/charlie.ics")
+    maxID, max = "Max", load_calendar("../calendars/max.ics")
+    charlieID, charlie = "Charlie", load_calendar("../calendars/charlie.ics")
+    hugoID, hugo = "Hugo", load_calendar("../calendars/hugo.ics")
 
-    fake_db = { maxID: max, charlieID: charlie }
+    fake_db = { maxID: max, charlieID: charlie, hugoID: hugo }
 
-    for i in get_breaks(max):
-        print(i.start, i.end)
+    # for i in get_breaks(max):
+    #     print(i.start, i.end)
 
-    print("-------------------------")
+    # print("-------------------------")
 
-    for i in get_breaks(charlie):
-        print(i.start, i.end)
+    # for i in get_breaks(charlie):
+    #     print(i.start, i.end)
 
-    get_breaks_now(maxID, fake_db)
+    breaks = get_breaks_now(maxID, fake_db)
+
+    for (friendID, brk) in breaks.items():
+        print(f"{friendID} has break starting at {brk.start} and ending at {brk.end}")
