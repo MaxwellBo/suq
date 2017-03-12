9aa358081e3059db2b17e530c8424c2e19ba76e9
charltongroves <charltongroves@gmail.com>
2017-03-12 17:21:07 +1000

    resetting tables

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
     db.session.commit()
-    db.drop_all() Uncomment to reset all tables.
+    db.drop_all()
     db.create_all()
     db.session.commit()
 
