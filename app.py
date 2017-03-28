ed2ae8a1d28ec341410bb98f707018b3d4d254d0
charltongroves <charltongroves@gmail.com>
2017-03-28 20:22:24 +1000

    Moved static and templates back to root

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ import requests
 
 ### GLOBALS ###
 
-app = Flask(__name__, static_folder='frontend/static', template_folder='frontend/templates')
+app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
