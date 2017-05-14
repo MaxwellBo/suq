d26d9cc4320086601a6967b8a06816f73a3c00f9
Max Bo <max@maxbo.me>
2017-05-14 19:10:59 +1000

    Fix typing error

--- backend/models.py
+++ backend/models.py
@@ -248,7 +248,7 @@ class User(db.Model, UserMixin):
         return { **user_details, **make_user_status("Unknown", "???")}
     
     @property
-    def availability(self, friend: User) -> Dict[str, str]:
+    def availability(self, friend) -> Dict[str, str]:
         breaks = get_remaining_shared_breaks_this_week([self, friend])
         self.status["breaks"] = [ i.to_dict() for i in breaks ]
 
