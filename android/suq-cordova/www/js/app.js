5f8f86430216bb800adcade0776a62c078f55a8f
Hugo Kawamata <hugokawamata@gmail.com>
2017-05-28 13:59:42 +1000

    Remove app from repo, fix links on landing page and login page

--- android/suq-cordova/www/js/app.js
+++
@@ -1,8 +0,0 @@
-function onDeviceReady() {
-    if (navigator.connection.type == Connection.NONE) {
-        navigator.notification.alert('An internet connection is required to continue');
-    } else {
-        window.open("http://syncuq.com", "_system");
-    }
-}
-document.addEventListener("deviceready", onDeviceReady, false);
